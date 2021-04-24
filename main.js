7f1f1a50c071de2d412133e3b7fd615beddf2934
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-04-24 23:13:13 +0300

    inital commit

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+//creating the constructor contact
+function Contact(firstName, lastName){
+ this.firstName = firstName;
+ this.lastName = lastName;
+}
+
+//first entry
+let metro = new Contact("Metro", "Axis");
+console.log(metro);
